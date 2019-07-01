88e9a9a9b5866ca7ad1760a7bfd348734114dc3c
ciandurrigan <cian.durrigan@gmail.com>
2019-07-01 13:37:12 +0000

    add JSX and JS to produce list of users favorite movies

--- src/App.js
+++ src/App.js
@@ -106,6 +106,18 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		<ul>
+          {profiles.map((profile) => {
+              const userName = users[profile.userID].name;
+              const favMovieName = movies[profile.favoriteMovieID].name;
+             
+			  return (
+                <li key={profile.id}>
+                	<span>{`${userName}'s favorite movie is ${favMovieName}`}</span>
+				</li>
+              )
+        })}
+		</ul>
       </div>
     );
   }
